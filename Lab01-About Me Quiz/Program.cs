728955d9d00aab973cd2e17e957c6b45f2a98006
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-09 11:26:39 -0700

    Question 3 complete

--- Lab01-About Me Quiz/Program.cs
+++ Lab01-About Me Quiz/Program.cs
@@ -6,8 +6,9 @@ namespace Lab01_About_Me_Quiz
     {
         static void Main(string[] args)
         {
-            Question1(); //calls the method for the first question; method returns boolean
-            Question2(); //calls the method for the second question; method returns boolean
+            Question1(); //calls the method for the first question; method returns a boolean
+            Question2(); //calls the method for the second question; method returns a boolean
+            Question3(); //calls the method for the third question; method returns an int
         }
 
         static bool Question1()
@@ -34,6 +35,40 @@ namespace Lab01_About_Me_Quiz
             Console.WriteLine(details);// displays the correct anwser with context to the user
 
             return parsedResponse; //returns either true or false
+        }
+
+        static int Question3()
+        {
+            string question = "Ok, let\'s see if you can guess my favorite number! Don\'t worry if you can\'t get it on the first try, you\'ll have four chances :).";
+            string details = "33 was my playing number throughout my AAU and high school basketball days.";
+            int userResponse;
+            int correctResponse = 33;
+            int numberGuesses = 4;
+            int j = 0;
+
+            Console.WriteLine(question); //asks the user a question in the console
+            do
+            {
+                Console.WriteLine("Enter a number, (" + (numberGuesses - j) + " guesses remain): ");
+                userResponse = int.Parse(Console.ReadLine());
+                j++;
+                if (userResponse == correctResponse)
+                {
+                    break;
+                }
+                if (userResponse < correctResponse)
+                {
+                    Console.WriteLine("Give it another shot, this time try something higher!");
+                }
+                if (userResponse > correctResponse)
+                {
+                    Console.WriteLine("Too high, try something a little lower!");
+                }
+            } while (j < numberGuesses && userResponse != correctResponse);
+
+            Console.WriteLine(details);// displays the correct anwser with context to the user
+
+            return userResponse;
         }
     }
 }
