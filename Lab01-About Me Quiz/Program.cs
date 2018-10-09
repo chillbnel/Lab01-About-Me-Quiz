a4d53cbae77a289136adcbf3bbe300e049e0b3ab
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-09 10:29:01 -0700

    Question 1 complete

--- Lab01-About Me Quiz/Program.cs
+++ Lab01-About Me Quiz/Program.cs
@@ -6,7 +6,17 @@ namespace Lab01_About_Me_Quiz
     {
         static void Main(string[] args)
         {
+            Question1();
+        }
 
+        static void Question1()
+        {
+            string question = "First question: Am I an only child? [True or False]";
+            string details = "I have a younger brother Eric.  He and I grew up battling each other on the N64, trading blows on Golden Eye and Mario Kart.";
+
+            Console.WriteLine(question); //asks the user a question in the console
+            string userResponse = Console.ReadLine().ToLower(); //takes user response, converts it to lower case and then stores it locally
+            Console.WriteLine(details);
         }
     }
 }
